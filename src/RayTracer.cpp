c4fa3f4270a06ecc5e1a444ad1c25d8ebe4eddc7
Adrien Tremblay <adrien@adrientremblay.com>
2023-02-22 21:29:00 -0500

    Mess around with Eigen3

--- src/RayTracer.cpp
+++ src/RayTracer.cpp
@@ -5,26 +5,35 @@
 #include <iostream>
 #include "RayTracer.h"
 #include "simpleppm.h"
+#include <Eigen/Dense>
 
 RayTracer::RayTracer(nlohmann::json& j) {
 
 }
 
 void RayTracer::run() {
+    Eigen::MatrixXd m(2, 2);
+    m(0, 0) = 3;
+    m(1, 0) = 2.5;
+    m(0, 1) = -1;
+    m(1, 1) = m(1, 0) + m(0, 1);
+
+    std::cerr << m << std::endl;
+
     // drawing an image
     const int image_width = 256;
     const int image_height = 256;
 
     std::vector<double> buffer(3*image_width*image_height);
     for (int j = 0 ; j < image_height ; j++) {
-        std::cerr << "\rScanlines remaining: " << image_height - j << std::flush;
+        //std::std << "\rScanlines remaining: " << image_height - j << std::flush;
         for (int i = 0 ; i < image_width ; i++){
             buffer[3*j*image_width+3*i+0] = double(i) / (image_width - 1);
             buffer[3*j*image_width+3*i+1] = double(j) / (image_height - 1);
             buffer[3*j*image_width+3*i+2] = 0.25;
         }
     }
-    std::cerr << "\nDone.\n";
+    //std::cerr << "\nDone.\n";
 
     save_ppm("adrien.ppm", buffer, image_width, image_height);
 }
