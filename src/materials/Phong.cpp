2483e75ea452f6f5f39a21407c73395c4b9846cb
Adrien Tremblay <adrien@adrientremblay.com>
2023-03-11 11:44:07 -0500

    Reverse direction of light_direction to simplify calculations

--- src/materials/Phong.cpp
+++ src/materials/Phong.cpp
@@ -2,6 +2,7 @@
 // Created by adrien on 10/03/23.
 //
 
+#include <iostream>
 #include "Phong.h"
 
 Phong::Phong(const Eigen::Vector3f &ambientColor, const Eigen::Vector3f &diffuseColor,
@@ -13,14 +14,12 @@ Eigen::Vector3f Phong::color(const Ray& rayIn, const HitRecord& hitRecord, const
     // todo: shitty code
     PointLight light = lights.at(0);
 
-    Eigen::Vector3f light_direction = (hitRecord.point - light.center).normalized();
+    Eigen::Vector3f light_direction = (light.center - hitRecord.point).normalized();
     float pc = 10;
 
-    float coeff = -light_direction.dot(hitRecord.normal);
-
     Eigen::Vector3f ambient = vector_multiply(light.ambientColor,  ambientCoeff * ambientColor);
-    Eigen::Vector3f diffuse = vector_multiply(light.diffuseColor, diffuseCoeff * coeff * diffuseColor);
-    Eigen::Vector3f specular = specularCoeff * pow(-(rayIn.getDirection().dot(reflect_vector(light_direction, hitRecord.normal))), pc) * specularColor;
+    Eigen::Vector3f diffuse = vector_multiply(light.diffuseColor, diffuseCoeff * light_direction.dot(hitRecord.normal) * diffuseColor);
+    Eigen::Vector3f specular = specularCoeff * pow(rayIn.getDirection().dot(reflect_vector(light_direction, hitRecord.normal)), pc) * specularColor;
 
-    return ambient + diffuse + specular;
+     return ambient + diffuse + specular;
 }
